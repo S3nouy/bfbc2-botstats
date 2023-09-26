64198b04f1ec563ed6427b4672a4e7fc9c6763ad
Senouy <77050462+S3nouy@users.noreply.github.com>
2023-09-26 12:49:29 +0300

    discord-bot.js

--- discord-bot.js
+++ discord-bot.js
@@ -3,71 +3,57 @@
  * Developed by Senouy aka CptYunsXD
  */
 
-require('dotenv').config(); // Load environment variables from .env file
-
+require('dotenv').config();
 const Discord = require('discord.js');
 const axios = require('axios');
 const client = new Discord.Client();
 const prefix = '!';
-
 client.on('message', async (message) => {
     if (message.author.bot) return;
-
     if (message.content.startsWith(prefix)) {
         const args = message.content.slice(prefix.length).trim().split(/ +/);
         const command = args.shift().toLowerCase();
-
         if (command === 'stats') {
-             const username = args.join(' ');
-
-            // Make a request to the Nexus API with the username
+            const username = args.join(' ');
+            
             try {
                 const apiUrl = `http://api.emulatornexus.com/v1/rome/persona/${username}/stats`;
                 const response = await axios.get(apiUrl);
+                
+                if (response.data.kills) {
+                    
+                    const stats = response.data;
+                    
+                    const kills = stats.kills;
+                    const deaths = stats.deaths;
+                    const kdRatio = deaths === 0 ? kills : (kills / deaths).toFixed(2);
 
-                // Parse the JSON response here and extract the relevant stats
-                const stats = parseStatsFromJSON(response.data.data);
-
-                // Calculate K/D ratio
-                const kills = stats.kills;
-                const deaths = stats.deaths;
-                const kdRatio = deaths === 0 ? kills : (kills / deaths).toFixed(2);
-
-                // Create an embed with the extracted stats
-                const embed = new Discord.MessageEmbed()
-                    .setTitle('Player Stats')
-                    .setColor('#0099ff')
-                    .addField('Player Name', username)
-                    .addField('Kills', kills, true)
-                    .addField('Deaths', deaths, true)
-                    .addField('K/D Ratio', kdRatio, true)
-                    .addField('Knife Kills', stats.knifeKills)
-                    .addField('Wins', stats.wins, true)
-                    .addField('Losses', stats.losses, true)
-                    .addField('Team Kills', stats.teamkills, true) // Add Team Kills field
-                    .setImage('https://i.ibb.co/6sZvccF/battlefield-bad-company-key-art-jpg-adapt-crop191x100-628p.jpg'); // Set the URL of the knife image
-
-                // Send the embed as a message
-                message.channel.send(embed);
+                    const embed = new Discord.MessageEmbed()
+                        .setTitle('Player Stats')
+                        .setColor('#0099ff')
+                        .addField('Player Name', username)
+                        .addField('Kills', kills, true)
+                        .addField('Deaths', deaths, true)
+                        .addField('K/D Ratio', kdRatio, true)
+                        .addField('Knife Kills', stats.c_knv__kw_g)
+                        .addField('Wins', stats.wins, true)
+                        .addField('Losses', stats.losses, true)
+                        .addField('Team Kills', stats.teamkills, true)
+                        .setImage('https://i.ibb.co/6sZvccF/battlefield-bad-company-key-art-jpg-adapt-crop191x100-628p.jpg');
+                    message.channel.send(embed);
+                } else {
+                    message.channel.send('Player stats not found.');
+                }
             } catch (error) {
                 console.error('Error fetching or parsing stats:', error);
                 message.channel.send('Error fetching player stats. Please try again later.');
             }
+        } else if (command === 'help') {
+            // Respond with a help message
+            const helpMessage = 'Here are the available commands:\n' +
+                '`!stats <username>` - Display player stats';
+            message.channel.send(helpMessage);
         }
     }
 });
-
-// Function to parse the stats from the JSON response
-function parseStatsFromJSON(data) {
-    return {
-        kills: data.kills,
-        deaths: data.deaths,
-        knifeKills: data.c_knv__kw_g,
-        wins: data.wins,
-        losses: data.losses,
-        teamkills: data.teamkills, // Add Team Kills property
-        // Add other stats properties here
-    };
-}
-
-client.login(process.env.BOT_TOKEN); // Use the BOT_TOKEN from the .env file
+client.login(process.env.BOT_TOKEN);
