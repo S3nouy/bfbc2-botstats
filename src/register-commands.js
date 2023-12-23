bfdd44e6fb8be4983678febb908a4ff79b926260
S3nouy <77050462+S3nouy@users.noreply.github.com>
2023-12-23 05:43:04 -0500

    Add files via upload

---
+++ src/register-commands.js
@@ -0,0 +1,64 @@
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
+
+const { ApplicationCommandOptionType } = require('discord.js');
+
+const commands = [
+  {
+    name: 'stats',
+    description: 'Displays player stats',
+    options: [
+      {
+        name: 'playername',
+        description: 'input a playername',
+        type: ApplicationCommandOptionType.String,
+        required: true,
+      },
+    ],
+  },
+  {
+    name: 'age',
+    description: 'Displays player age',
+    options: [
+      {
+        name: 'playername',
+        description: 'input a playername',
+        type: ApplicationCommandOptionType.String,
+        required: true,
+      },
+    ],
+  },
+      {
+    name: 'score',
+    description: 'Displays player score',
+    options: [
+      {
+        name: 'playername',
+        description: 'input a playername',
+        type: ApplicationCommandOptionType.String,
+        required: true,
+      },
+    ],
+  },
+]; // <-- Add this closing bracket
+
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('Registering slash commands...');
+
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: commands }
+    );
+
+    console.log('Slash commands were registered successfully!');
+  } catch (error) {
+    console.log(`There was an error: ${error}`);
+  }
+})();
